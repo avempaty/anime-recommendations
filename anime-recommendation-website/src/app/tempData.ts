6aa9a51ed712a36e6a2c96975a51570d1f85d7ea
Aneesh Vempaty <aneesh.vempaty@gmail.com>
2025-03-02 13:15:35 -0800

    Made anime cards

---
+++ anime-recommendation-website/src/app/tempData.ts
@@ -0,0 +1,102 @@
+export const data = [
+    {
+        node: {
+            id: 38101,
+            title: "5-toubun no Hanayome",
+            main_picture: {
+                medium: "https://cdn.myanimelist.net/images/anime/1819/97947.webp",
+                large: "https://cdn.myanimelist.net/images/anime/1819/97947l.webp",
+            },
+        },
+    },
+    {
+        node: {
+            id: 41457,
+            title: "86",
+            main_picture: {
+                medium: "https://cdn.myanimelist.net/images/anime/1987/117507.webp",
+                large: "https://cdn.myanimelist.net/images/anime/1987/117507l.webp",
+            },
+        },
+    },
+    {
+        node: {
+            id: 48569,
+            title: "86 Part 2",
+            main_picture: {
+                medium: "https://cdn.myanimelist.net/images/anime/1321/117508.jpg",
+                large: "https://cdn.myanimelist.net/images/anime/1321/117508l.jpg",
+            },
+        },
+    },
+    {
+        node: {
+            id: 49235,
+            title: "86 Special Edition: Senya ni Akaku Hinageshi no Saku",
+            main_picture: {
+                medium: "https://cdn.myanimelist.net/images/anime/1971/115976.jpg",
+                large: "https://cdn.myanimelist.net/images/anime/1971/115976l.jpg",
+            },
+        },
+    },
+    {
+        node: {
+            id: 32998,
+            title: "91 Days",
+            main_picture: {
+                medium: "https://cdn.myanimelist.net/images/anime/13/80515.jpg",
+                large: "https://cdn.myanimelist.net/images/anime/13/80515l.jpg",
+            },
+        },
+    },
+    {
+        node: {
+            id: 22199,
+            title: "Akame ga Kill!",
+            main_picture: {
+                medium: "https://cdn.myanimelist.net/images/anime/1429/95946.webp",
+                large: "https://cdn.myanimelist.net/images/anime/1429/95946l.webp",
+            },
+        },
+    },
+    {
+        node: {
+            id: 25013,
+            title: "Akatsuki no Yona",
+            main_picture: {
+                medium: "https://cdn.myanimelist.net/images/anime/9/64225.jpg",
+                large: "https://cdn.myanimelist.net/images/anime/9/64225l.jpg",
+            },
+        },
+    },
+    {
+        node: {
+            id: 41433,
+            title: "Akudama Drive",
+            main_picture: {
+                medium: "https://cdn.myanimelist.net/images/anime/1468/109172.jpg",
+                large: "https://cdn.myanimelist.net/images/anime/1468/109172l.jpg",
+            },
+        },
+    },
+    {
+        node: {
+            id: 22729,
+            title: "Aldnoah.Zero",
+            main_picture: {
+                medium: "https://cdn.myanimelist.net/images/anime/7/60263.webp",
+                large: "https://cdn.myanimelist.net/images/anime/7/60263l.webp",
+            },
+        },
+    },
+    {
+        node: {
+            id: 27655,
+            title: "Aldnoah.Zero Part 2",
+            main_picture: {
+                medium: "https://cdn.myanimelist.net/images/anime/10/71297.webp",
+                large: "https://cdn.myanimelist.net/images/anime/10/71297l.webp",
+            },
+        },
+    },
+];
